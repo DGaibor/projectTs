9b3fcdc06ac550450368c4ceab759f29d69d4dd6
Diego Gaibor <diego.g@scopicsoftware.com>
2025-12-01 19:26:11 -0500

    auth middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,12 +1,48 @@
 import type{ Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { UserService } from '../user/user.service';
+interface MyJwtPayload extends JwtPayload {
+    email: string;
+    name: string;
+}
+
+
 
 export const timeLog = ( req:Request,res: Response, next:NextFunction )=>{
     console.log('Middleware timeLog', Date.now());
+
     next();
 }
 
+
 export const privateRoute = ( req:Request,res: Response, next:NextFunction )=>{
-    const token = req.headers.authorization;
-    console.log(token);
+    const token  = req.headers.authorization;
+    if(!token){
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+    
+    const realToken: string = token?.replace('Bearer ', '') ?? '';
+    const secretKey:string = process.env.SECRET_KEY ?? '';
+    
+    try{
+        const decode =  jwt.verify(realToken,secretKey) as MyJwtPayload;
+        const userService = new UserService();
+        const user = userService.getUserByEmail(decode.email)
+        
+        if(user == null){
+           return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+
+        req.body.user = user;
+    }catch (e) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        })
+    }
+
     next();
 }
